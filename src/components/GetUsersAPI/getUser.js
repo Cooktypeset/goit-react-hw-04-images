3dfa2fa24b97c99ac476d484195b4a50e2763430
Cooktypeset <tovpyhalitva@gmail.com>
2023-08-29 18:06:11 +0300

    finishUpdicon

--- src/components/GetUsersAPI/getUser.js
+++ src/components/GetUsersAPI/getUser.js
@@ -1,4 +1,4 @@
-const API_KEY = '36028425-48f97f4e693396b4b6c5eb90a';
+const API_KEY = '37979441-f1d7e4c3b344b48b7a2dc7a50';
 
 export default async function getInfoFromApi(value, page) {
   const URL = `https://pixabay.com/api/?q=${value}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
